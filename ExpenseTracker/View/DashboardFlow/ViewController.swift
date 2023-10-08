b63d32c414104e87dcb5f05c570f2c80e7cf5590
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2023-10-08 03:39:10 +0300

    Set dashboard UI

--- ExpenseTracker/View/DashboardFlow/ViewController.swift
+++ ExpenseTracker/View/DashboardFlow/ViewController.swift
@@ -11,6 +11,16 @@ import DGCharts
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var donationStackView: UIStackView!
+    @IBOutlet weak var entertainmentStackView: UIStackView!
+    @IBOutlet weak var foodStackView: UIStackView!
+    @IBOutlet weak var healthStackView: UIStackView!
+    @IBOutlet weak var shoppingStackView: UIStackView!
+    @IBOutlet weak var transportionStackView: UIStackView!
+    @IBOutlet weak var utilitiesStackView: UIStackView!
+    @IBOutlet weak var otherStackView: UIStackView!
+    
     @IBOutlet weak var donationPriceLabel: UILabel!
     @IBOutlet weak var entertainmentPrieceLabel: UILabel!
     @IBOutlet weak var foodPriceLabel: UILabel!
@@ -23,15 +33,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var pieView: PieChartView!
     
     @IBOutlet weak var totalPriceLabel: UILabel!
+    
     let colors: [UIColor] = [
-        UIColor(hex: 0xFF5733),
-        UIColor(hex: 0xE74C3C),
-        UIColor(hex: 0x3498DB),
-        UIColor(hex: 0x1ABC9C),
-        UIColor(hex: 0x9B59B6),
-        UIColor(hex: 0xF39C12),
-        UIColor(hex: 0x16A085),
-        UIColor(hex: 0xF39C12)
+        UIColor(hex: 0x007AFF),
+        UIColor(hex: 0xFF9500),
+        UIColor(hex: 0x4CD964),
+        UIColor(hex: 0xFF2D55),
+        UIColor(hex: 0x5856D6),
+        UIColor(hex: 0xFFCC00),
+        UIColor(hex: 0x34C759),
+        UIColor(hex: 0xFF3B30)
     ]
     
     var viewModel = DashboardVM()
@@ -44,32 +55,16 @@ class ViewController: UIViewController {
         
         setTabBar()
         setupPieChart()
+    
         
-        let donationTotal = calculateCategoryTotal(category: "Donation", prices: viewModel.priceArray)
-        donationPriceLabel.text = donationTotal != 0 ? "$\(String(donationTotal))" : "$0"
-        donationPriceLabel.textColor = UIColor(hex: 0xFF5733)
-        
-        
-        let entertainmentTotal = calculateCategoryTotal(category: "Entertainment", prices: viewModel.priceArray)
-        entertainmentPrieceLabel.text = entertainmentTotal != 0 ? "$\(String(entertainmentTotal))" : "$0"
-        
-        let foodTotal = calculateCategoryTotal(category: "Food", prices: viewModel.priceArray)
-        foodPriceLabel.text = foodTotal != 0 ? "$\(String(foodTotal))" : "$0"
-        
-        let healthTotal = calculateCategoryTotal(category: "Health", prices: viewModel.priceArray)
-        healthPriceLabel.text = healthTotal != 0 ? "$\(String(healthTotal))" : "$0"
-        
-        let shoppingTotal = calculateCategoryTotal(category: "Shopping", prices: viewModel.priceArray)
-        shoppingPriceLabel.text = shoppingTotal != 0 ? "$\(String(shoppingTotal))" : "$0"
-        
-        let transportionTotal = calculateCategoryTotal(category: "Transportion", prices: viewModel.priceArray)
-        transportainPriceLabel.text = transportionTotal != 0 ? "$\(String(transportionTotal))" : "$0"
-        
-        let utilitiesTotal = calculateCategoryTotal(category: "Utilities", prices: viewModel.priceArray)
-        utilitiesPriceLabel.text = utilitiesTotal != 0 ? "$\(String(utilitiesTotal))" : "$0"
-        
-        let otherTotal = calculateCategoryTotal(category: "Other", prices: viewModel.priceArray)
-        otherPriceLabel.text = otherTotal != 0 ? "$\(String(otherTotal))" : "$0"
+        setCategoryStackView(stackView: donationStackView, colorHex: colors[0])
+        setCategoryStackView(stackView: entertainmentStackView, colorHex: colors[1])
+        setCategoryStackView(stackView: foodStackView, colorHex: colors[2])
+        setCategoryStackView(stackView: healthStackView, colorHex: colors[3])
+        setCategoryStackView(stackView: shoppingStackView, colorHex: colors[4])
+        setCategoryStackView(stackView: transportionStackView, colorHex: colors[5])
+        setCategoryStackView(stackView: utilitiesStackView, colorHex: colors[6])
+        setCategoryStackView(stackView: otherStackView, colorHex: colors[7])
 
         
         self.tabBarController!.tabBar.layer.borderWidth = 0.50
@@ -91,7 +86,7 @@ class ViewController: UIViewController {
         viewModel.getAllData()
     }
     
-    func calculateCategoryTotal(category: String, prices: [Int]) -> Int{
+    func calculateCategoryTotal(category: String, prices: [Int]) -> Double{
         let filteredPrices = prices.filter { $0 != 0 }
             var total = 0
             
@@ -101,10 +96,9 @@ class ViewController: UIViewController {
                 }
             }
             
-            return total
+        return Double(total)
 
     }
-    
     
     func setTabBar(){
         self.tabBarController?.tabBar.items?[0].image = UIImage(named: "circleChart")?.withRenderingMode(.alwaysOriginal)
@@ -114,10 +108,51 @@ class ViewController: UIViewController {
         
     }
     
+    func setCategoryStackView(stackView: UIStackView, colorHex: UIColor){
+        stackView.layer.borderColor = colorHex.cgColor
+        stackView.layer.borderWidth = 1
+        stackView.layer.cornerRadius = 5
+        
+    }
+    
     func setupPieChart(){
         var dataEntries: [ChartDataEntry] = []
         
-        let values: [Double] = [10.0, 10.0, 20.0, 10.0, 10.0, 10.0, 20.0, 10.0]
+        let donationTotal = calculateCategoryTotal(category: "Donation", prices: viewModel.priceArray)
+        donationPriceLabel.text = donationTotal != 0 ? "$\(String(donationTotal))" : "$0"
+//        donationPriceLabel.textColor = UIColor(hex: 0x007AFF)
+        
+        let entertainmentTotal = calculateCategoryTotal(category: "Entertainment", prices: viewModel.priceArray)
+        entertainmentPrieceLabel.text = entertainmentTotal != 0 ? "$\(String(entertainmentTotal))" : "$0"
+//        entertainmentPrieceLabel.textColor = UIColor(hex: 0xFF9500)
+        
+        let foodTotal = calculateCategoryTotal(category: "Food", prices: viewModel.priceArray)
+        foodPriceLabel.text = foodTotal != 0 ? "$\(String(foodTotal))" : "$0"
+        foodPriceLabel.textColor = UIColor(hex: 0x4CD964)
+        
+        let healthTotal = calculateCategoryTotal(category: "Health", prices: viewModel.priceArray)
+        healthPriceLabel.text = healthTotal != 0 ? "$\(String(healthTotal))" : "$0"
+        healthPriceLabel.textColor = UIColor(hex: 0xFF2D55)
+        
+        let shoppingTotal = calculateCategoryTotal(category: "Shopping", prices: viewModel.priceArray)
+        shoppingPriceLabel.text = shoppingTotal != 0 ? "$\(String(shoppingTotal))" : "$0"
+        shoppingPriceLabel.textColor = UIColor(hex: 0x5856D6)
+        
+        let transportionTotal = calculateCategoryTotal(category: "Transportion", prices: viewModel.priceArray)
+        transportainPriceLabel.text = transportionTotal != 0 ? "$\(String(transportionTotal))" : "$0"
+        transportainPriceLabel.textColor = UIColor(hex: 0xFFCC00)
+        
+        let utilitiesTotal = calculateCategoryTotal(category: "Utilities", prices: viewModel.priceArray)
+        utilitiesPriceLabel.text = utilitiesTotal != 0 ? "$\(String(utilitiesTotal))" : "$0"
+        utilitiesPriceLabel.textColor = UIColor(hex: 0x34C759)
+        
+        let otherTotal = calculateCategoryTotal(category: "Other", prices: viewModel.priceArray)
+        otherPriceLabel.text = otherTotal != 0 ? "$\(String(otherTotal))" : "$0"
+        otherPriceLabel.textColor = UIColor(hex: 0xFF3B30)
+        
+        let values: [Double] = [donationTotal, entertainmentTotal, foodTotal, healthTotal, shoppingTotal, transportionTotal, utilitiesTotal, otherTotal]
+        
+       
         for (index, value) in values.enumerated() {
             let entry = PieChartDataEntry(value: value, label: "Segment \(index)")
             dataEntries.append(entry)
